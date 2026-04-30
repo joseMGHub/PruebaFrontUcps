3b7d0c134251e4ea7a5664c31546c232f0786923
joseBoo7 <jmamanigo@unsa.edu.pe>
2026-04-29 22:47:11 -0500

    Update_Role_Gestor

---
+++ assets/fileApi-VBK1Y15H.js
@@ -0,0 +1,1 @@
+import{a,E as e}from"./Endpoints-BHVV8Jxm.js";const n=async s=>(await a.post(e.STORAGE.FILE.UPLOAD,s)).data,p=async s=>(await a.post(e.STORAGE.FILE.UPDATE,s)).data,r=async s=>(await a.post(e.STORAGE.FILE.DELETE,s)).data;export{p as a,r,n as u};
